f752f64f18fe1f45f46208deae43746ec48f6d62
Yahor Radtsevich <yradtsevich@exadel.com>
2011-12-09 13:40:44 +0000

    https://issues.jboss.org/browse/JBIDE-10424 : CLONE - BrowserSim icon is active on *all* perspectives - added an actionSet for browsersim and replaced browsersim command handler by the same action handler.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@37163 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ plugins/org.jboss.tools.vpe.browsersim.eclipse/src/org/jboss/tools/vpe/browsersim/eclipse/actions/RunBrowserSimAction.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2011 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.vpe.browsersim.eclipse.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.jboss.tools.vpe.browsersim.eclipse.util.BrowserSimLauncher;
+
+/**
+ * @author "Yahor Radtsevich (yradtsevich)"
+ */
+public class RunBrowserSimAction implements IWorkbenchWindowActionDelegate {
+
+	@Override
+	public void run(IAction action) {
+		BrowserSimLauncher.launchBrowserSim(null);
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+	@Override
+	public void init(IWorkbenchWindow window) {
+	}
+}
