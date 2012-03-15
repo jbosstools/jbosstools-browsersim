84308162a493cdb90c0f0987f261595309f00b1d
Yahor Radtsevich <yradtsevich@exadel.com>
2012-03-15 17:12:54 +0000

    https://issues.jboss.org/browse/JBIDE-11191 - BrowserSim Web Page Title - should change to the <title></title> - OS X adaptation
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@39544 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.vpe.browsersim/src/org/jboss/tools/vpe/browsersim/ui/skin/ios/IOSPageTitleStyledText.java
+++ plugins/org.jboss.tools.vpe.browsersim/src/org/jboss/tools/vpe/browsersim/ui/skin/ios/IOSPageTitleStyledText.java
@@ -15,6 +15,7 @@ import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Device;
 import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.jboss.tools.vpe.browsersim.ui.skin.ImageList;
 
@@ -22,32 +23,32 @@ public class IOSPageTitleStyledText extends StyledText {
 	private final String BD = "ios/";
 	private Font font;
 	private Color foregroundColor;
+	private static boolean IS_OSX = SWT.getPlatform() == "cocoa";
 	
 	public IOSPageTitleStyledText(Composite parent, ImageList imageList) {
-		super(parent, SWT.SINGLE | SWT.WRAP);
+		super(parent, (IS_OSX ? SWT.MULTI : SWT.SINGLE) | SWT.WRAP);
 		setBackgroundImage(imageList.getImage(BD + "page-title-background.png"));
 		setEditable(false);
 		setEnabled(false);
-		setMargins(16, 6, 16, 0);
+		setMargins(16, IS_OSX ? 7 : 6, 16, 0);
 		setAlignment(SWT.CENTER);
-		font = createFont(this.getDisplay(), 9, SWT.BOLD);
+		setLineSpacing(27);
+		font = createFont(this.getDisplay(), SWT.BOLD);
 		setFont(font);
 		foregroundColor = new Color(this.getDisplay(), 64, 64, 64);
 		setForeground(foregroundColor);
 	}
 	
-	static Font createFont(Device device, int FONT_SIZE, int style) {
+	static Font createFont(Device device, int style) {
 		Font font;
 		if(SWT.getPlatform() == "win32") {
-			font = new Font(device, "Arial", FONT_SIZE, style);	
-		} else if (SWT.getPlatform() == "motif") {
-			font = new Font(device, "Times", FONT_SIZE, style);		
+			font = new Font(device, "Arial", 9, style);	
 		} else if (SWT.getPlatform() == "gtk") {
-			font = new Font(device, "Baekmuk Batang", FONT_SIZE, style);		
+			font = new Font(device, "Baekmuk Batang", 9, style);		
 		} else if (SWT.getPlatform() == "cocoa") {
-			font = new Font(device, "Helvetica", FONT_SIZE, style);
-		} else { // photon
-			font = new Font(device, "Verdana", FONT_SIZE, style);
+			font = new Font(device, "Helvetica", 12, style);
+		} else {
+			font = new Font(device, "Verdana", 9, style);
 		}
 		
 		return font;
