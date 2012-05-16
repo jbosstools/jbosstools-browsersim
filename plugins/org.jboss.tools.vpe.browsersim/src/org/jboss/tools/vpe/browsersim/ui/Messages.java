ccd3e47bc820330ba4c5a0a8e46f002b42e040e1
Yahor Radtsevich <yradtsevich@exadel.com>
2012-05-16 11:39:47 +0000

    https://issues.jboss.org/browse/JBIDE-11739 : browsersim does not set the right height if monitor has lower resolution - should handle/warn about it - sorted Messages
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@41058 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.vpe.browsersim/src/org/jboss/tools/vpe/browsersim/ui/Messages.java
+++ plugins/org.jboss.tools.vpe.browsersim/src/org/jboss/tools/vpe/browsersim/ui/Messages.java
@@ -17,6 +17,7 @@ import org.jboss.tools.vpe.browsersim.util.NLS;
  */
 public class Messages {
 	private static final String BUNDLE_NAME = Messages.class.getName().toString().toLowerCase();
+	
 	public static String BrowserSim_ADDRESS;
 	public static String BrowserSim_BROWSER_SIM;
 	public static String BrowserSim_COULD_NOT_OPEN_DEFAULT_BROWSER;
@@ -42,39 +43,39 @@ public class Messages {
 	public static String EditDeviceDialog_WIDTH;
 	public static String ExceptionNotifier_APPLE_APPLICATION_SUPPORT_IS_NOT_FOUND;
 	public static String ExceptionNotifier_BROWSERSIM_IS_FAILED_TO_START;
-	public static String ExceptionNotifier_ONLY_32_BIT_ECLIPSE_IS_SUPPORTED_ON_WINDOWS;
 	public static String ExceptionNotifier_BROWSERSIM_IS_FAILED_TO_START_ON_LINUX;
+	public static String ExceptionNotifier_OK;
+	public static String ExceptionNotifier_ONLY_32_BIT_ECLIPSE_IS_SUPPORTED_ON_WINDOWS;
 	public static String ManageDevicesDialog_ADD;
+	public static String ManageDevicesDialog_ALWAYS_TRUNCATE;
 	public static String ManageDevicesDialog_CANCEL;
 	public static String ManageDevicesDialog_DEFAULT;
 	public static String ManageDevicesDialog_DEVICES;
 	public static String ManageDevicesDialog_EDIT;
 	public static String ManageDevicesDialog_HEIGHT;
+	public static String ManageDevicesDialog_LOAD_DEFAULTS;
 	public static String ManageDevicesDialog_NAME;
-	public static String ManageDevicesDialog_NONE;
+	public static String ManageDevicesDialog_NEVER_TRUNCATE;
 	public static String ManageDevicesDialog_NEW_DEVICE;
 	public static String ManageDevicesDialog_NEW_USER_AGENT;
+	public static String ManageDevicesDialog_NONE;
 	public static String ManageDevicesDialog_OK;
 	public static String ManageDevicesDialog_PREFERENCES;
+	public static String ManageDevicesDialog_PROMPT;
 	public static String ManageDevicesDialog_REMOVE;
 	public static String ManageDevicesDialog_REVERT_ALL;
 	public static String ManageDevicesDialog_SKIN;
-	public static String ManageDevicesDialog_LOAD_DEFAULTS;
+	public static String ManageDevicesDialog_SKINS_OPTIONS;
+	public static String ManageDevicesDialog_TRUNCATE_THE_DEVICE_WINDOW;
+	public static String ManageDevicesDialog_USE_SKINS;
 	public static String ManageDevicesDialog_USER_AGENT;
 	public static String ManageDevicesDialog_WIDTH;
-	public static String SizeWarningDialog_DEVICE_SIZE_WILL_BE_TRUNCATED;
-	public static String SizeWarningDialog_DESKTOP_SIZE_TOO_SMALL_VERTICAL;
-	public static String SizeWarningDialog_DESKTOP_SIZE_TOO_SMALL_HORIZONTAL;
-	public static String SizeWarningDialog_REMEMBER_MY_DECISION;
-	public static String SizeWarningDialog_OK;
 	public static String SizeWarningDialog_CANCEL;
-	public static String ManageDevicesDialog_SKINS_OPTIONS;
-	public static String ManageDevicesDialog_USE_SKINS;
-	public static String ManageDevicesDialog_TRUNCATE_THE_DEVICE_WINDOW;
-	public static String ManageDevicesDialog_ALWAYS_TRUNCATE;
-	public static String ManageDevicesDialog_NEVER_TRUNCATE;
-	public static String ManageDevicesDialog_PROMPT;
-	public static String ExceptionNotifier_OK;
+	public static String SizeWarningDialog_DESKTOP_SIZE_TOO_SMALL_HORIZONTAL;
+	public static String SizeWarningDialog_DESKTOP_SIZE_TOO_SMALL_VERTICAL;
+	public static String SizeWarningDialog_DEVICE_SIZE_WILL_BE_TRUNCATED;
+	public static String SizeWarningDialog_OK;
+	public static String SizeWarningDialog_REMEMBER_MY_DECISION;
 
 	static {
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
