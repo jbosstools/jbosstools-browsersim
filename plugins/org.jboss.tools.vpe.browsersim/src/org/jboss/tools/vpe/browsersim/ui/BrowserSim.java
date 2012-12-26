41708845b54985ffee8da585a348329290d39e34
Konstantin Marmalyukov <kmarmaliykov@exadel.com>
2012-12-26 20:57:29 +0300

    JBIDE-13338 BrowserSim: integrate weinre functionality to BrowserSim
    adding functionality

--- plugins/org.jboss.tools.vpe.browsersim/src/org/jboss/tools/vpe/browsersim/ui/BrowserSim.java
+++ plugins/org.jboss.tools.vpe.browsersim/src/org/jboss/tools/vpe/browsersim/ui/BrowserSim.java
@@ -21,6 +21,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.UUID;
 
 import javax.xml.bind.DatatypeConverter;
 
@@ -50,6 +51,7 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.program.Program;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
@@ -454,7 +456,7 @@ public class BrowserSim {
 				addDevicesMenuItems(contextMenu);
 				addUseSkinsItem(contextMenu);
 				addPreferencesItem(contextMenu);
-					
+				
 				new MenuItem(contextMenu, SWT.BAR);
 				addTurnMenuItems(contextMenu);
 
@@ -463,6 +465,9 @@ public class BrowserSim {
 				
 				new MenuItem(contextMenu, SWT.BAR);
 				addFileMenuItems(contextMenu);
+				
+				new MenuItem(contextMenu, SWT.BAR);
+				addWeinreItem(contextMenu);
 				
 				new MenuItem(contextMenu, SWT.BAR);
 				addAboutItem(contextMenu);
@@ -597,6 +602,40 @@ public class BrowserSim {
 						System.out.println(base64Source);
 					}
 				}
+			}
+		});
+	}
+	
+	public void addWeinreItem(Menu menu) {
+		MenuItem weinre = new MenuItem(menu, SWT.PUSH);
+		weinre.setText(Messages.BrowserSim_WEINRE);
+		weinre.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				String url = (String) skin.getBrowser().evaluate("if(window.WeinreServerURL && window.WeinreServerId) {return window.WeinreServerURL + 'client/#' + window.WeinreServerId} else {return null}");
+				if (url == null) {
+					String id = UUID.randomUUID().toString();
+					skin.getBrowser().execute("var head = document.head;"
+							+	"var script = document.createElement('script');"
+							+	"head.appendChild(script);" 
+							+	"script.src='http://debug.phonegap.com/target/target-script-min.js#" + id + "'");
+					
+					url = "http://debug.phonegap.com/client/#" + id;
+				}
+				
+				Display display = skin.getBrowser().getDisplay();
+				Shell shell = new Shell(display);
+				shell.setLayout(new FillLayout());
+				shell.setText("Weinre Inspector");
+				final Browser browser;
+				try {
+					browser = new Browser(shell, SWT.WEBKIT);
+				} catch (SWTError e2) {
+					System.out.println("Could not instantiate Browser: " + e2.getMessage());
+					display.dispose();
+					return;
+				}
+				shell.open();
+				browser.setUrl(url);
 			}
 		});
 	}
@@ -959,7 +998,6 @@ public class BrowserSim {
 		}
 		return new Shell();
 	}
-
 	
 	private void addMacOsMenuApplicationHandler(CocoaUIEnhancer enhancer) {
 		
