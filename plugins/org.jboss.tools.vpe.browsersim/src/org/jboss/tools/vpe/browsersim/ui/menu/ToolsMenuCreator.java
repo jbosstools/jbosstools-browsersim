79ac37e6a11d66798977354c84d5d4b7c421d2ee
Konstantin Marmalyukov <kmarmaliykov@exadel.com>
2014-05-08 21:27:50 +0300

    JBIDE-16930 Livereload doesn't work with BrowserSim and JavaFX Browser Engine if running against JDK 7
    livereload disabled for java7 javafx engine

--- plugins/org.jboss.tools.vpe.browsersim/src/org/jboss/tools/vpe/browsersim/ui/menu/ToolsMenuCreator.java
+++ plugins/org.jboss.tools.vpe.browsersim/src/org/jboss/tools/vpe/browsersim/ui/menu/ToolsMenuCreator.java
@@ -26,7 +26,6 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.program.Program;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Menu;
